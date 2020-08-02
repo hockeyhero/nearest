2d798c0f7a73f995973a3e425f433c8915571aca
David Ewan <davidewan@live.com>
2020-08-02 12:42:43 -0400

    Updating config.yml

--- enc.cmd
+++ enc.cmd
@@ -1,3 +1,2 @@
 :: to encrypt: enc + password
-::set path=%path%;%~dp0openssl-1.1.1\bin
-openssl-1.1.1\bin\openssl enc -aes-256-ecb -pbkdf2 -md md5 -in .\src\main\resources\application.properties -out .\src\main\resources\application.properties.secrets -k %1
+openssl-1.1.1\bin\openssl enc -aes-256-ecb  -md md5 -in .\src\main\resources\application.properties -out .\src\main\resources\application.properties.secrets -k %1
