81cab79a9faa82ca959ec9e93de911a4f5701009
David Ewan <davidewan@live.com>
2020-08-02 12:42:43 -0400

    Updating config.yml

--- dec.cmd
+++ dec.cmd
@@ -1,2 +1,2 @@
 :: to decrypt: dec + password
-openssl-1.1.1\bin\openssl enc -d -aes-256-ecb -pbkdf2 -md md5 -in .\src\main\resources\application.properties.secrets -out .\src\main\resources\application.properties -k %1
+openssl-1.1.1\bin\openssl enc -d -aes-256-ecb  -md md5 -in .\src\main\resources\application.properties.secrets -out .\src\main\resources\application.properties -k %1
