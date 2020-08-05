4feb52608169c9de86ae8e675584c7e9ee8ac706
David Ewan <davidewan@live.com>
2020-08-04 22:13:54 -0400

    Move application.properties down into a config folder

--- enc.cmd
+++ enc.cmd
@@ -1,2 +1,2 @@
 :: to encrypt: enc + password
-openssl-1.1.1\bin\openssl enc -aes-256-ecb  -md md5 -in .\src\main\resources\application.properties -out .\src\main\resources\application.properties.secrets -k %1
+openssl-1.1.1\bin\openssl enc -aes-256-ecb  -md md5 -in .\src\main\resources\config\application.properties -out .\src\main\resources\config\application.properties.secrets -k %1
